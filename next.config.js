c25a48fa5ecc5ded25a238c6f8c38b0d80ef77ff
yangyang <yangyang@ainnovation.com>
2024-07-19 10:24:31 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,26 +1,33 @@
 /**
  * @type {import('next').NextConfig}
  */
+try {
+  const nextConfig = {
+    reactStrictMode: true,
+    output:'export', // from NextJs v13.3.0 next export is depricated
+    images: {
+      unoptimized: true, // mandatory, otherwise won't export
+    },
+    basePath: "",
+    //Optional: Change the output directory `out` -> `dist`
+  //  distDir: "build"
 
-const nextConfig = {
-  reactStrictMode: true,
-  output:'export', // from NextJs v13.3.0 next export is depricated
-  images: {
-    unoptimized: true, // mandatory, otherwise won't export
-  },
-  basePath: "",
-  //Optional: Change the output directory `out` -> `dist`
-//  distDir: "build"
-
-}
-
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './src/theme.config.js',
-  latex: true,
-  flexsearch: {
-    codeblock: false
   }
-})
 
-module.exports = withNextra(nextConfig) //export the config as a dependency
+  const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './src/theme.config.js',
+    latex: true,
+    flexsearch: {
+      codeblock: false
+    }
+  })
+
+  module.exports = withNextra(nextConfig) //export the config as a dependency
+} catch (err) {
+  if (!(err instanceof Error)) {
+    throw new Error('An unknown error occurred');
+  }
+  console.log("err: ", err)
+  throw err;
+}
